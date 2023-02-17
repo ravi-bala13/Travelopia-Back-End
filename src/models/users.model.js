a2a8807e6fb9a2f87d37890f5019a061d0c86584
karthick-pandi <93534549+karthick-pandi@users.noreply.github.com>
2023-02-17 07:46:42 +0530

    added cors

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
         email: {type: String, required: true, unique: true},
         place: {type: String, required: false},
         no_of_travellers: {type: Number, required: false},
-        currency: {type: Number, required: false}
+        amount: {type: Number, required: false}
     }
 
 );
